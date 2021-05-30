aefc96d22c10e392d7de6565f2cb089a9b15abcd
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-30 03:31:04 +0300

    Copy serialize_post() as serialize_post_optimized()

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,20 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_post(post):
     return {
         'title': post.title,
@@ -41,9 +55,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
