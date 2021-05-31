823f605129e2311c210f5d0b55b76c5802531d6e
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-31 11:16:54 +0300

    Fix code using PEP8

--- blog/models.py
+++ blog/models.py
@@ -15,12 +15,14 @@ class PostQuerySet(models.QuerySet):
 
     def fetch_with_comments_count(self):
         posts_ids = [post.id for post in self]
-        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments_count=Count('comments'))
+        posts_with_comments = Post.objects.filter(id__in=posts_ids) \
+                                          .annotate(comments_count=Count('comments'))
         ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
         count_for_id = dict(ids_and_comments)
         for post in self:
             post.comments_count = count_for_id[post.id]
         return self
+
 
 class TagQuerySet(models.QuerySet):
 
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.db.models import Count
 from django.db.models import Prefetch
 
 
-def serialize_post_optimized(post):
+def serialize_post(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -13,15 +13,8 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag_optimized(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': tag.posts__count
     }
 
 
@@ -36,11 +29,18 @@ def index(request):
 
     most_popular_posts = Post.objects.popular()[:5] \
                                      .prefetch_related('author') \
-                                     .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(Count('posts')))) \
+                                     .prefetch_related(
+                                         Prefetch(
+                                             'tags',
+                                             queryset=Tag.objects.annotate(Count('posts')))) \
                                      .fetch_with_comments_count()
 
-    fresh_posts = Post.objects.order_by('published_at').prefetch_related('author') \
-                              .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(Count('posts')))) \
+    fresh_posts = Post.objects.order_by('published_at') \
+                              .prefetch_related('author') \
+                              .prefetch_related(
+                                  Prefetch(
+                                      'tags',
+                                      queryset=Tag.objects.annotate(Count('posts')))) \
                               .fetch_with_comments_count()
 
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -49,9 +49,9 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
@@ -88,14 +88,17 @@ def post_detail(request, slug):
 
     most_popular_posts = Post.objects.popular()[:5] \
                                      .prefetch_related('author') \
-                                     .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(Count('posts')))) \
+                                     .prefetch_related(
+                                         Prefetch(
+                                             'tags',
+                                             queryset=Tag.objects.annotate(Count('posts')))) \
                                      .fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
@@ -108,20 +111,26 @@ def tag_filter(request, tag_title):
 
     most_popular_posts = Post.objects.popular() \
                                      .prefetch_related('author')[:5] \
-                                     .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(Count('posts')))) \
+                                     .prefetch_related(
+                                         Prefetch(
+                                             'tags',
+                                             queryset=Tag.objects.annotate(Count('posts')))) \
                                      .fetch_with_comments_count()
 
     related_posts = tag.posts.order_by('-published_at')[:20] \
                        .prefetch_related('author') \
-                       .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(Count('posts')))) \
+                       .prefetch_related(
+                           Prefetch(
+                               'tags',
+                               queryset=Tag.objects.annotate(Count('posts')))) \
                        .fetch_with_comments_count()
 
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'posts': [serialize_post_optimized(post) for post in related_posts],
+        'posts': [serialize_post(post) for post in related_posts],
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, 'posts-list.html', context)
